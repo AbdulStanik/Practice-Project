a5811765f800372167068d0d845db431bd9bb084
AbdulStanik <93871851+AbdulStanik@users.noreply.github.com>
2021-11-13 09:02:51 -0600

    Add files via upload

---
+++ BooleanClass.java
@@ -0,0 +1,15 @@
+package seleniumPackage;
+
+public class BooleanClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       boolean b =true;
+       System.out.println("Afghanistan is the symbol of beauty:- " + b);
+       System.out.println("Pakistan is a terrorist country:- " + b);
+       boolean b1 =false;
+       System.out.println("Java is a browser:- " + b1);
+       
+	}
+
+}
